45f5755a9cd8adbbbb492b1d9d5e9285583b19c1
mrjuoss <mrjuoss@gmail.com>
2017-10-18 09:39:55 +0700

    Login from database with session

--- 05-database/login_submit.php
+++ 05-database/login_submit.php
@@ -2,9 +2,34 @@
 
 session_start();
 
+include "config.php";
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+$sql = "SELECT * FROM member WHERE member_username = '".$username."' AND member_password = '".$password."' ";
+/* Debug Perintah SQL untuk mengetahui SQL nya sudah benar atau tidak
+    echo $sql;
+    exit();
+*/
+$query = $koneksi->query($sql);
+
+if ($query->num_rows > 0)
+{
+  $rows  = $query->fetch_assoc();
+  $_SESSION['sess_user']  = $rows['member_username'];
+  $_SESSION['nama_user'] = $rows['member_name'];
+
+  header ("location:index.php");
+} else {
+  header ("location:index.php?err=1");
+}
+
+// echo $user;
+// echo "<br>".$pass;
+// exit();
+
+/* Check Login manual (Bukan dari Database)
 if ($username == 'admin' && $password == 'admin')
 {
   $_SESSION["sess_user"] = $username;
@@ -14,5 +39,6 @@ if ($username == 'admin' && $password == 'admin')
 } else {
   header ("location:index.php?err=1");
 }
+*/
 
 ?>
